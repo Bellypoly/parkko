2ac510c2cc918d5c6ec932a0ffb8a01983eed320
teerapat <teerapat.kao@gmail.com>
2015-08-28 15:07:14 +0700

    Fix short tag

--- receipt/list.php
+++ receipt/list.php
@@ -1,4 +1,4 @@
-<?
+<?php
 include("../var.php");
 ?>
 <html>
@@ -8,14 +8,14 @@ include("../var.php");
 		<link rel="stylesheet" href="../css/boon.css">
 	</head>
 	<body>
-		<? foreach ($data as  $record){?>
-		<a href="show.php?id=<?=$record["id"];?>" target="_blank" style="text-decoration: none; font-weight:bold;">
+		<?php foreach ($data as  $record){?>
+		<a href="show.php?id=<?php echo $record["id"];?>" target="_blank" style="text-decoration: none; font-weight:bold;">
 		<div style="border: 2px solid #000; background-color:#fff; width:200px; text-align:center; padding:20px;">
-			<?=$record["license"];?><br/>
-			<?=$record["province"];?>
+			<?php echo $record["license"];?><br/>
+			<?php echo $record["province"];?>
 		</div>
 		</a>
 		<div style="height:30px;"></div>
-		<?}?>
+		<?php }?>
 	</body>
-</html>
+</html> 
