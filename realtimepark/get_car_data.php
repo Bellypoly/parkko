1de2a99d4b147d9bb1e1e5a4f1fe52709d0c58a4
burin-mbp <burin@thinknet.co.th>
2015-08-28 14:13:06 +0700

    count hours

--- realtimepark/get_car_data.php
+++ realtimepark/get_car_data.php
@@ -1,11 +1,13 @@
 <?php
 include '../var.php';
+include 'cal_hour.php';
 $park_car = [
     'ณข1549ลำปาง' => 'B145'
 ];
 
 function get_data($license = '', $province = '')
 {
+    date_default_timezone_set("Asia/Bangkok");
     foreach ($GLOBALS['data'] as $val) {
         if($val['license'] == $license && $val['province'] == $province) {
             $temp = $val;
@@ -17,7 +19,9 @@ function get_data($license = '', $province = '')
     $result['park'] = $park_car[$license . $province];
     $result['time_in'] = $temp['time_in'];
     $result['time_now'] = date('Y/m/d H:i:s');
+    $result['hours'] = countHours($result['time_in'], $result['time_now']);
+    var_dump($result);
 }
 
 //example
-//get_data('ณข1549', 'ลำปาง');
+get_data('ณข1549', 'ลำปาง');
